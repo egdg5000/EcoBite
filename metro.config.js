3281cc18d543ae35a770413260c80b432e2d2152
BryanKeislair <bryankeislair@gmail.com>
2025-03-19 12:24:44 +0100

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-
-const { getDefaultConfig } = require('expo/metro-config');
-
-const DefaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push('cjs');
-
-module.exports = defaultConfig;
