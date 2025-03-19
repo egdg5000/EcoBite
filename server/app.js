bd3e6495b015eca1d03abecd66b5d2d906f574a5
edg5000 <emmetdegraaf@gmail.com>
2025-03-19 08:52:55 +0100

    Sessions now get stored in the db instead of memory

--- server/app.js
+++ server/app.js
@@ -4,14 +4,25 @@ const cors = require('cors')
 const app = express();
 const port = 3000;
 const dotenv = require('dotenv').config()
+const MySQLStore = require('express-mysql-session')(session);
 
 app.use(cors({
   origin: 'http://localhost:8081',
   credentials: true
 }));
 
+const sessionStore = new MySQLStore({
+  host: 'localhost',
+	port: 3306,
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: process.env.SESSION_DB
+});
+
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+	key: 'session_cookie_name',
+	secret: 'session_cookie_secret',
+  store: sessionStore,
   resave: false,
   saveUninitialized: false,
   cookie: { 
@@ -19,6 +30,12 @@ app.use(session({
     maxAge: 7 * 24 * 60 * 60 * 1000 //7 days
   }
 }));
+
+sessionStore.onReady().then(() => {
+	console.log('MySQLSessionStore ready');
+}).catch(error => {
+	console.error(error);
+});
 
 //Routes
 app.use("/users", require("./routes/users.js"));
