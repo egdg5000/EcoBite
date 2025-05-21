7af1fa0083a5e947ab07905a3e8780a50c23b474
edg5000 <emmetdegraaf@gmail.com>
2025-05-21 09:56:03 +0200

    fix error

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../database");
+const {db} = require("../database");
 
 // ➕ Voeg nieuw product toe
 router.post("/add", async (req, res) => {
