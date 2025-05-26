aefa8f2f65ba16a0381681d609fceb7f62cec068
BryanKeislair <bryankeislair@gmail.com>
2025-05-26 14:20:49 +0200

    Update help.tsx

--- frontend/app/help.tsx
+++ frontend/app/help.tsx
@@ -27,7 +27,7 @@ export default function HelpPage() {
                     
                     <TouchableOpacity style={styles.question}>
                         <Text style={styles.questionText}>🔍 Hoe scan ik een barcode?</Text>
-                        <Text style={styles.answerText}>Ga naar de Homepagina en kies 'Scan Barcode' wanneer u op de groene + knop drukt. Geef wanneer nodig toestemming voor het gebruiken van de camera en richt uw camera op de barcode. Zorg ervoor dat de barcode duidelijk in beeld is.</Text>
+                        <Text style={styles.answerText}>Ga naar de Homepagina en kies 'Scan Barcode' wanneer u op de groene + knop drukt. Geef wanneer nodig toestemming voor het gebruiken van de camera en richt uw camera op de barcode of het product. Zorg ervoor dat de barcode of product duidelijk in beeld is. Als het product successvol is gescand wordt het automatisch toegevoegd aan uw voorraad.</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity style={styles.question}>
@@ -38,6 +38,11 @@ export default function HelpPage() {
                     <TouchableOpacity style={styles.question}>
                         <Text style={styles.questionText}>🍽️ Hoe krijg ik recepten op basis van mijn ingrediënten?</Text>
                         <Text style={styles.answerText}>Open de voorraad en selecteer de beschikbare ingrediënten. De app geeft suggesties voor recepten op basis van wat u heeft. U kunt toegevoegde ingrediënten ook toevoegen aan de favorieten, zodat u ze sneller kan selecteren voor een volgende keer.</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.question}>
+                        <Text style={styles.questionText}>🎮 Hoe krijg ik XP en zorg ik voor een mooiere boom?</Text>
+                        <Text style={styles.answerText}>Wanneer u met behulp van de toegevoegde producten recepten gaat maken krijgt u XP en reduceert u een bepaalde hoeveelheid CO2. U gaat immers producten die u nog over heeft niet verspillen, wat ons doel van deze app is. Hoe de boom eruit ziet hangt af van de hoeveelheid CO2 u heeft bespaard. Hoe meer dit is, hoe beter de boom eruit ziet.</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity style={styles.question}>
@@ -55,7 +60,7 @@ export default function HelpPage() {
                 <View style={styles.section}>
                     <Text style={styles.sectionTitle}>Extra ondersteuning</Text>
                     <Text style={styles.text}>Heeft u nog vragen? Neem contact met ons op via:</Text>
-                    <Text style={styles.contactText}>📧 placeholder@ecobite.com</Text>
+                    <Text style={styles.contactText}>📧 ecobite@edg5000.com</Text>
                 </View>
             </ScrollView>
         </SafeAreaView>
