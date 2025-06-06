c69c50ff21d5279644839119c40fa1404fd7343b
edg5000 <emmetdegraaf@gmail.com>
2025-06-06 11:59:02 +0200

    fix db port

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(
 
 const sessionStore = new MySQLStore({
   host: 'localhost',
-  port: 3307,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.SESSION_DB,
